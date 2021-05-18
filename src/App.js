a0c4e932106ce7d3161155d5f30eb5c581a8cda1
Maulik Shah <maulik.shah715@gmail.com>
2021-05-18 12:32:16 +0530

    🧬 Added the Header and made a basic dropdown also

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import "./App.css";
 import React from "react";
+import { FormControl, MenuItem, Select } from "@material-ui/core";
 
 function App() {
-  return <div className="app">
-    <h1>Let's build a COVID 19 tracker!</h1>
-  </div>;
+  return (
+    <div className="app">
+      <div className="app__header">
+        <h1>COVID 19 Tracker</h1>
+        
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            <MenuItem value="worldwide">WorldWide</MenuItem>
+            <MenuItem value="worldwide">Op 2</MenuItem>
+            <MenuItem value="worldwide">option three</MenuItem>
+            <MenuItem value="worldwide">option 4 here</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+
+      {/* HEADER */}
+      {/* TITLE + SELECT INPUT DROPDOWN FIELDS */}
+
+      {/* INFO BOXES */}
+      {/* INFO BOXES */}
+      {/* INFO BOXES */}
+
+      {/* TABLE */}
+      {/* GRAPH */}
+
+      {/* MAP */}
+    </div>
+  );
 }
 
 export default App;
