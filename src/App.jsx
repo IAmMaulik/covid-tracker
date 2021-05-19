97fbd996b2d9ac6f5026eee2d0c27d520b33b808
Maulik Shah <maulik.shah715@gmail.com>
2021-05-19 13:01:00 +0530

    🧣 Styling is now perfect and we can continue with the rest of the stuff

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,39 @@ import "./App.css";
 import React from "react";
 import Dropdown from "./components/Dropdown";
 import Infobox from "./components/Infobox";
+import Map from "./components/Map";
+import { Card, CardContent } from "@material-ui/core";
 
 function App() {
   return (
     <div className="app">
-      {/* Header */}
-      <div className="app__header">
-        <h1>COVID 19 Tracker</h1>
-        <Dropdown />
+      <div className="app__left">
+        {/* Header */}
+        <div className="app__header">
+          <h1>COVID 19 Tracker</h1>
+          <Dropdown />
+        </div>
+
+        {/* INFO BOXES */}
+        <div className="app__stats">
+          <Infobox title="Coronavirus Cases" cases="1234" total="235245123" />
+          <Infobox title="Recovered" cases="1234" total="235245123" />
+          <Infobox title="Deaths" cases="1234" total="235245123" />
+        </div>
+
+        {/* MAP */}
+        <Map />
       </div>
 
-      {/* INFO BOXES */}
-      <div className="app__stats">
-        <Infobox title="Coronavirus Cases" cases="1234" total="235245123" />
-        <Infobox title="Recovered" cases="1234" total="235245123" />
-        <Infobox title="Deaths" cases="1234" total="235245123" />
-      </div>
+      <Card className="app__right">
+        <CardContent>
+          {/* TABLE */}
+          <h3>Live new cases</h3>
 
-      {/* TABLE */}
-      {/* GRAPH */}
-
-      {/* MAP */}
+          {/* GRAPH */}
+          <h3>Worldwide new cases</h3>
+        </CardContent>
+      </Card>
     </div>
   );
 }
