3fe06487c6a5a64180ca04c737bbc837bb342400
Maulik Shah <maulik.shah715@gmail.com>
2021-05-23 12:45:46 +0530

    🥑 The service worker is now functional and the app is now a PWA

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -3,19 +3,18 @@ var urlsToCache = ["/", "/completed", "/build"];
 
 const self = this;
 
-// Install a service worker
-self.addEventListener("install", installEvent => {
+self.addEventListener("install", (installEvent) => {
   installEvent.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      cache.addAll(urlsToCache)
+    caches.open(CACHE_NAME).then((cache) => {
+      cache.addAll(urlsToCache);
     })
-  )
-})
+  );
+});
 
-self.addEventListener("fetch", fetchEvent => {
+self.addEventListener("fetch", (fetchEvent) => {
   fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request)
+    caches.match(fetchEvent.request).then((res) => {
+      return res || fetch(fetchEvent.request);
     })
-  )
-})
+  );
+});
